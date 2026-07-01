a6b55da6a1a167c5cfd22fe448d7b437818bb4b7
patrick espinoza <patrickespinoza38@gmail.com>
2026-06-30 21:16:00 -0600

    cambio de formato

--- Nuestra-Boda/src/main.jsx
+++ Nuestra-Boda/src/main.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import Portada from './portada'
 import Intinerario from './intinerario'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <main>
     <div >
-      <Portada/>
       <Intinerario/>
     </div>
   </main>
